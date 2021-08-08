8ab6f29a3dc07c79a8f3547aef54f1a7dfa88df8
Johannes Hackbarth <joha@informatik.uni-bremen.de>
2021-08-08 18:40:48 +0200

    Don't start joint state publisher This is taken care of by gazebo. Camera image now shows up in rviz2.

--- pilsbot_simulation/launch/pilsbot.launch.py
+++ pilsbot_simulation/launch/pilsbot.launch.py
@@ -27,11 +27,6 @@ def generate_launch_description():
                              output='both',
                              parameters=[params])
 
-    joint_states = Node(name='joint_state_publisher',
-                        package='joint_state_publisher',
-                        executable='joint_state_publisher',
-                        output='screen')
-
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', '/robot_description', '-entity', 'pilsbot'],
                         output='screen')
@@ -39,6 +34,5 @@ def generate_launch_description():
     return LaunchDescription([
         gazebo,
         robot_description,
-        joint_states,
         spawn_entity,
     ])
