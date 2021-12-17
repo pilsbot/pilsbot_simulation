f6ac5fc810e4f5046943c3c6e52ceca62723317d
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2021-12-17 12:36:01 +0100

    Add arguments to control test launch file

--- pilsbot_simulation/launch/pilsbot_control_test.launch.py
+++ pilsbot_simulation/launch/pilsbot_control_test.launch.py
@@ -1,27 +1,72 @@
-import os
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, TextSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    declared_launch_args = []
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'controller_config', default_value=TextSubstitution(text='acker_diff_controller.yaml'),
+        description='The controller configuration you want to use.'))
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'descritption_file', default_value=TextSubstitution(text='pilsbot.urdf.xacro'),
+        description='URDF/XACRO description file with the robot.'))
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'description_package', default_value=TextSubstitution(text='pilsbot_description'),
+        description=' Description package with robot URDF/xacro files. Usually the argument \
+        is not set, it enables use of a custom description.'))
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'world', default_value=TextSubstitution(text='')
+    ))
+
+    declared_launch_args.append(DeclareLaunchArgument(
+        'verbose', default_value=TextSubstitution(text='false'),
+        description='Set Gazebo output to verbose.'
+    ))
+
+    # intialise args
+    controller_config = LaunchConfiguration('controller_config')
+    description_file = LaunchConfiguration('descritption_file')
+    description_package = LaunchConfiguration('description_package')
+    world = LaunchConfiguration('world')
+    verbose = LaunchConfiguration('verbose')
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [FindPackageShare("gazebo_ros"), '/launch/gazebo.launch.py']),
+        launch_arguments={'world': world,
+                          'verbose': verbose}.items()
     )
 
-    pkg_share = FindPackageShare('pilsbot_description').find('pilsbot_description')
-    urdf_dir = os.path.join(pkg_share, 'urdf')
-    xacro_file = os.path.join(urdf_dir, 'pilsbot.urdf.xacro')
-    robot_desc = Command('xacro {}'.format(xacro_file))
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare(description_package),
+                    "urdf",
+                    description_file,
+                ]
+            ),
+            " ",
+            "controller_config:=",
+            controller_config,
+            " ",
+            "use_fake_hardware:=true",
+        ]
+    )
 
-    params = {'robot_description': robot_desc,
+    params = {'robot_description': robot_description_content,
               'use_sim_time': use_sim_time}
 
     robot_description = Node(package='robot_state_publisher',
@@ -29,10 +74,9 @@ def generate_launch_description():
                              output='both',
                              parameters=[params])
 
-
-
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', '/robot_description', '-entity', 'pilsbot'],
+                        arguments=['-topic', '/robot_description',
+                                   '-entity', 'pilsbot'],
                         output='screen')
 
     spawn_dd_controller = Node(
@@ -48,10 +92,11 @@ def generate_launch_description():
         output="screen",
     )
 
-    return LaunchDescription([
-        gazebo,
-        robot_description,
-        spawn_entity,
-        spawn_dd_controller,
-        spawn_jsb_controller,
-    ])
+    nodes = [gazebo,
+             robot_description,
+             spawn_entity,
+             spawn_dd_controller,
+             spawn_jsb_controller,
+             ]
+
+    return LaunchDescription(declared_launch_args + nodes)
